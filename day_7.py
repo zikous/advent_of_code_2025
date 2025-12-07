602649925742a2062acfc960e7986aaffc31ec42
Zakaria Bheddar <zakaria.bheddar@student-cs.fr>
2025-12-07 10:09:55 +0100

    Refactor Day 7 solution to use defaultdict for active timelines and simplify timeline updates

--- day_7.py
+++ day_7.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 with open("inputs/7.txt") as f:
     grid = [list(line.strip()) for line in f]
 
@@ -11,24 +13,25 @@ for i in range(n):
             s_i, s_j = i, j
             break
 
-active = {s_j: 1}  # dict mapping column -> number of timelines reaching this column
+active = defaultdict(int)
+active[s_j] = 1  # dict mapping column -> number of timelines reaching this column
 res1 = 0  # total splitter hits
 
 # Process the grid row by row
 for r in range(s_i + 1, n):
-    new_active = {}
+    new_active = defaultdict(int)
     for c, count in active.items():
         if grid[r][c] == "^":
             res1 += 1  # splitter hit
 
             # split into two timelines: left and right
             if c - 1 >= 0:
-                new_active[c - 1] = new_active.get(c - 1, 0) + count
+                new_active[c - 1] += count
             if c + 1 < m:
-                new_active[c + 1] = new_active.get(c + 1, 0) + count
+                new_active[c + 1] += count
         else:
             # timeline continues straight down
-            new_active[c] = new_active.get(c, 0) + count
+            new_active[c] += count
     active = new_active
 
 res2 = sum(active.values())
